bf2155e5aac82dbc853a6693f5de7c4f5137d2ce
VIKAS POTE <102276688+vikaspote07@users.noreply.github.com>
2024-12-10 10:10:03 +0530

    Update StepIndicator.jsx
    modified stepindicatot

--- front-end/src/componants/StepIndicator.jsx
+++ front-end/src/componants/StepIndicator.jsx
@@ -4,12 +4,14 @@ export function StepIndicator({ currentStep, totalSteps }) {
   return (
     <div className="flex justify-between mb-4">
       {[...Array(totalSteps)].map((_, index) => (
-        <div
+         <div
           key={index}
-          className={`w-4 h-4 rounded-full ${
-            currentStep === index + 1 ? "bg-blue-600" : "bg-gray-400"
-          }`}
-        ></div>
+          className={`w-6 h-6 rounded-full ${
+            currentStep === index + 1 ? "bg-blue-600 text-white" : "bg-gray-400"
+          } flex justify-center items-center text-black-700`}
+        >
+          {index + 1}
+        </div>
       ))}
     </div>
   );
